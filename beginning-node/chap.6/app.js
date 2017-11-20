dc9a4bab5ba291b1cfcc83cb2124876334a965da
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-20 08:01:46 -0800

    more with servers and sending an html file as a response

--- beginning-node/chap.6/app.js
+++ beginning-node/chap.6/app.js
@@ -1,12 +1,21 @@
-const http = require('http');
+const http 	= require('http');
+const fs	= require('fs');
+
+function send404(response) {
+	response.writeHead(404, {'Content-Type' : 'text/plain'});
+	response.write('Error 404: Resource not found.');
+	response.end();
+};
+
 
 let server = http.createServer((req, res) => {
-	console.log('requesting headers...');
-	console.log(req.headers);
-
-	// respond
-	res.write('hello client!');
-	res.end();
+	if( req.method == 'GET' && req.url == '/' ) {
+		res.writeHead(200, { 'Content-Type' : 'text/html' });
+		fs.createReadStream('./public/index.html').pipe(res);
+	}
+	else {
+		send404(res);
+	}
 })
 
 server.listen(3000);
