d6cdfe09c673dbcd69dc8c741a17a3247573fd23
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-20 08:33:36 -0800

    exploring with connect

--- beginning-node/chap.6/app.js
+++ beginning-node/chap.6/app.js
@@ -1,6 +1,7 @@
-const http 	= require('http');
-const fs	= require('fs');
-const path	= require('path');
+const connect 	= require('connect');
+const http 		= require('http');
+const fs		= require('fs');
+const path		= require('path');
 
 
 function send404(response) {
