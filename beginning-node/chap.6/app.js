0c79b561056f7aeda2d7a4842a2d5bfbeadb379a
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-20 08:29:23 -0800

    serving a client side javasctipt liked with in the html file

--- beginning-node/chap.6/app.js
+++ beginning-node/chap.6/app.js
@@ -1,5 +1,7 @@
 const http 	= require('http');
 const fs	= require('fs');
+const path	= require('path');
+
 
 function send404(response) {
 	response.writeHead(404, {'Content-Type' : 'text/plain'});
@@ -7,11 +9,46 @@ function send404(response) {
 	response.end();
 };
 
+const mimeLookup = {
+	'.js' : 'application/javascript',
+	'.html': 'text/html'
+};
+
 
 let server = http.createServer((req, res) => {
-	if( req.method == 'GET' && req.url == '/' ) {
-		res.writeHead(200, { 'Content-Type' : 'text/html' });
-		fs.createReadStream('./public/index.html').pipe(res);
+	if(req.method == 'GET') {
+
+		//resolve file path to file system path
+		var fileurl;
+
+		if(req.url == '/') {
+			fileurl = './index.html';
+		} else {
+			fileurl = req.url;
+		}
+		var filepath = path.resolve('./public/' + fileurl);
+
+		// lookup mimetype
+		var fileExt = path.extname(filepath);
+		var mimeType = mimeLookup[fileExt];
+
+		if(!mimeType) {
+			send404(res);
+			return;
+		}
+
+		// see if we have that file
+		fs.exists(filepath, function(exists) {
+			// if not 
+			if(!exists) {
+				send404(res);
+				return;
+			};
+
+			// finally stream the file
+			res.writeHead(200, {'Content-Type': mimeType});
+			fs.createReadStream(filepath).pipe(res);
+		});
 	}
 	else {
 		send404(res);
---
+++ beginning-node/chap.6/public/main.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+	document.body.innerHTML += '<strong>Talk Javascript with me!</strong>';
+	console.log('here')
+};
+
