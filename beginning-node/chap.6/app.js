c5061d242c25632fe53be206409af0c5fed94fdc
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-20 08:46:24 -0800

    more with connect and util

--- beginning-node/chap.6/app.js
+++ beginning-node/chap.6/app.js
@@ -2,59 +2,14 @@ const connect 	= require('connect');
 const http 		= require('http');
 const fs		= require('fs');
 const path		= require('path');
+const util		= require('util');
 
+// a simple logging middleware
+function logit(req, res, next) {
+	util.log(util.format('Request recieved: %s, %s', req.method, req.url));
+	next();
+}
 
-function send404(response) {
-	response.writeHead(404, {'Content-Type' : 'text/plain'});
-	response.write('Error 404: Resource not found.');
-	response.end();
-};
-
-const mimeLookup = {
-	'.js' : 'application/javascript',
-	'.html': 'text/html'
-};
-
-
-let server = http.createServer((req, res) => {
-	if(req.method == 'GET') {
-
-		//resolve file path to file system path
-		var fileurl;
-
-		if(req.url == '/') {
-			fileurl = './index.html';
-		} else {
-			fileurl = req.url;
-		}
-		var filepath = path.resolve('./public/' + fileurl);
-
-		// lookup mimetype
-		var fileExt = path.extname(filepath);
-		var mimeType = mimeLookup[fileExt];
-
-		if(!mimeType) {
-			send404(res);
-			return;
-		}
-
-		// see if we have that file
-		fs.exists(filepath, function(exists) {
-			// if not 
-			if(!exists) {
-				send404(res);
-				return;
-			};
-
-			// finally stream the file
-			res.writeHead(200, {'Content-Type': mimeType});
-			fs.createReadStream(filepath).pipe(res);
-		});
-	}
-	else {
-		send404(res);
-	}
-})
-
-server.listen(3000);
-console.log('server is running at http://127.0.0.1:3000');
+connect()
+	.use(logit)
+	.listen(3000)
