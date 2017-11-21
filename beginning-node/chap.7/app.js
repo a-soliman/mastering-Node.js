e1e331e1380f5be2ae98d124ebb779ecdf0f9ba8
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-21 08:36:31 -0800

    working with cookies

--- beginning-node/chap.7/app.js
+++ beginning-node/chap.7/app.js
@@ -2,20 +2,10 @@ const express		= require('express');
 const bodyParser	= require('body-parser');
 
 let app = express()
-	.use(bodyParser.urlencoded({ extended: false }))
-	.use(bodyParser.json())
 	.use((req, res) => {
-		console.log(req.body)
-		if( req.body.foo ) {
-			res.end('Body parsed! value of foo: ' + req.body.foo);
-		}
-		else {
-			res.end('Body does not have foo!');
-		}
+		res.cookie({'name': 'foo'});
+		res.end(req.cookie);
 	})
-	.use((err, req, res, next) => {
-		res.end('Invalid Body!')
-	})
-	.listen(3000, () => {
-		console.log('Server is running on 3000...')
+	.listen(3001, () => {
+		console.log('Server is running at 3000.')
 	})
