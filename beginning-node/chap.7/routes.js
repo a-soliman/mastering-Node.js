df95cef5c395eca2652f677a4a5fdc25f9affebf
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-22 07:27:40 -0800

    routes object

--- beginning-node/chap.7/routes.js
+++ beginning-node/chap.7/routes.js
@@ -2,26 +2,48 @@ const express 	= require('express');
 
 let app = express();
 
-app.all('/', (req, res, next) => {
-	res.write('all\n');
-	next();
-});
+// app.all('/', (req, res, next) => {
+// 	res.write('all\n');
+// 	next();
+// });
 
-app.get('/', (req, res, next) => {
-	res.end('get');
-});
+// app.get('/', (req, res, next) => {
+// 	res.end('get');
+// });
 
-app.post('/', (req, res, next) => {
-	res.end('post')
-});
+// app.post('/', (req, res, next) => {
+// 	res.end('post')
+// });
 
-app.put('/', (req, res, next) => {
-	res.end('put')
-});
+// app.put('/', (req, res, next) => {
+// 	res.end('put')
+// });
 
-app.delete('/', (req, res, next) => {
-	res.end('delete')
-});
+// app.delete('/', (req, res, next) => {
+// 	res.end('delete')
+// });
+
+
+
+// === routes object ===
+app.route('/')
+	.all((req, res, next) => {
+		res.write('all\n');
+		next();
+	})
+	.post((req, res, next) => {
+		res.end('post');
+	})
+	.get((req, res, next) => {
+		res.end('get');
+	})
+	.put((req, res, next) => {
+		res.end('put');
+	})
+	.delete((req, res, next) => {
+		res.end('delete');
+	});
+
 
 app.listen(3000, () => {
 	console.log('server is runing on 3000.')
