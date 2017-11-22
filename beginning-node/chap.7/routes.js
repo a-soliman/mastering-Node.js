162a12eebd0a7789e6170e0e37e64fe5308c0780
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-22 07:21:12 -0800

    working with routes in express

---
+++ beginning-node/chap.7/routes.js
@@ -0,0 +1,28 @@
+const express 	= require('express');
+
+let app = express();
+
+app.all('/', (req, res, next) => {
+	res.write('all\n');
+	next();
+});
+
+app.get('/', (req, res, next) => {
+	res.end('get');
+});
+
+app.post('/', (req, res, next) => {
+	res.end('post')
+});
+
+app.put('/', (req, res, next) => {
+	res.end('put')
+});
+
+app.delete('/', (req, res, next) => {
+	res.end('delete')
+});
+
+app.listen(3000, () => {
+	console.log('server is runing on 3000.')
+})
