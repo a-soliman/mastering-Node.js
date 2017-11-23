260e4c12c136369deea7d6af41186fcb32669f23
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-23 08:19:51 -0800

    working with remote mongo db, Mlab

---
+++ beginning-node/chap.8/app.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient;
+
+let demoPerson = { name: 'Ahmed', lastName: 'Soliman'};
+let findKey = { name : 'Ahmed'};
+
+let mongoUrl = 'mongodb://ahmed_soliman:abc123@ds119306.mlab.com:19306/beginning-node';
+
+MongoClient.connect(mongoUrl, (err, db) => {
+	if(err) throw err;
+	console.log('successfully connected');
+
+	let collection = db.collection('people');
+
+	collection.insert(demoPerson, (err, docs) => {
+		console.log('inserted', docs[0]);
+		console.log(docs.ops[0]);
+		console.log('ID: ', demoPerson._id)
+	})
+})
