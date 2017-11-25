4b860a8798311dd67eb59434d94466e99a6d61c1
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-25 10:35:40 -0800

    more with mongoose and mongo putting them all together

--- beginning-node/chap.8/mongoose.js
+++ beginning-node/chap.8/mongoose.js
@@ -1,22 +1,63 @@
 const mongoose = require('mongoose');
 
 
-// Schema
+// all together
+
+//Schema
 let tankSchema = new mongoose.Schema({ name: String, size: String});
 tankSchema.methods.print = function() {
-	console.log(`Hello, I'm ${this.name}, of size ${this.size}`);
+	console.log(`I'm ${this.name} the ${this.size}`);
 };
 
 let Tank = mongoose.model('Tank', tankSchema);
 
+//create the connection
+const mongoUrl = 'mongodb://ahmed_soliman:abc123@ds119306.mlab.com:19306/beginning-node';
 
+mongoose.connect(mongoUrl);
+let db = mongoose.connection;
+db.once('open', () => {
+	console.log('connected!');
+});
+
+// use the model
 let tony = new Tank({ name: 'Tony', size: 'small'});
+tony.print()
 
+// save 
 tony.save((err) => {
 	if(err) throw err;
 
-	//saved
-})
+	console.log('Saved!')
+});
+
+//find
+
+
+
+// // Schema
+// let tankSchema = new mongoose.Schema({ name: String, size: String});
+// tankSchema.methods.print = function() {
+// 	console.log(`Hello, I'm ${this.name}, of size ${this.size}`);
+// };
+
+// let Tank = mongoose.model('Tank', tankSchema);
+
+
+// let tony = new Tank({ name: 'Tony', size: 'small'});
+// let dz = new Tank({ name: 'DZ', size: 'meduim'});
+
+// // tony.save((err) => {
+// // 	if(err) throw err;
+
+// // 	//saved
+// // })
+// Tank.findOne({name: 'DZ'})
+// 	.exec((err, tank) => {
+// 		if(err) throw err;
+
+// 		tank.print();
+// 	})
 
 
 // // connecting via mongoose
