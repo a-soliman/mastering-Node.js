be07094d6768933e022046f6ae84670ea0c690ee
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-25 10:22:37 -0800

    working with mongoose models and Schema

--- beginning-node/chap.8/mongoose.js
+++ beginning-node/chap.8/mongoose.js
@@ -1,16 +1,35 @@
 const mongoose = require('mongoose');
 
-// connecting via mongoose
-const mongoUrl = 'mongodb://ahmed_soliman:abc123@ds119306.mlab.com:19306/beginning-node';
 
-mongoose.connect(mongoUrl);
+// Schema
+let tankSchema = new mongoose.Schema({ name: String, size: String});
+tankSchema.methods.print = function() {
+	console.log(`Hello, I'm ${this.name}, of size ${this.size}`);
+};
 
-let db = mongoose.connection;
-db.on('error', (err) => {
-	throw err;
-});
+let Tank = mongoose.model('Tank', tankSchema);
 
-db.once('open', () => {
-	console.log('connected');
-	db.close();
+
+let tony = new Tank({ name: 'Tony', size: 'small'});
+
+tony.save((err) => {
+	if(err) throw err;
+
+	//saved
 })
+
+
+// // connecting via mongoose
+// const mongoUrl = 'mongodb://ahmed_soliman:abc123@ds119306.mlab.com:19306/beginning-node';
+
+// mongoose.connect(mongoUrl);
+
+// let db = mongoose.connection;
+// db.on('error', (err) => {
+// 	throw err;
+// });
+
+// db.once('open', () => {
+// 	console.log('connected');
+// 	db.close();
+// })
