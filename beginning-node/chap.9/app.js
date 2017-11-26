c7b8cb4ed494a384990c1ceaca8316ef99948d00
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-26 12:57:53 -0800

    added the needed libraries to the FE.

---
+++ beginning-node/chap.9/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+let app = express();
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.listen(3000, () => {
+	console.log('server is running on port 3000');
+});
