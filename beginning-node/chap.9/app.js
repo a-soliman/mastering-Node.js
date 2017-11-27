d9076554e5c95a30cfbf0691180efa65e0770d97
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-27 08:35:33 -0800

    coded the server rooutes to get, post and remove an item of the ToDo list

--- beginning-node/chap.9/app.js
+++ beginning-node/chap.9/app.js
@@ -1,10 +1,65 @@
-const express = require('express');
+const express 		= require('express');
+const bodyParser 	= require('body-parser');
+
+
 
 let app = express();
-app.use(express.static(__dirname + '/public'));
 
+// create MongoDB connection
+const MongoClient = require('mongodb').MongoClient;
+let db;
+let itemsCollection;
 
+const mongoUrl = 'mongodb://ahmed_soliman:abc123@ds119306.mlab.com:19306/beginning-node';
+MongoClient.connect(mongoUrl, function(err, database) {
+	if (err) throw err;
 
-app.listen(3000, () => {
+	//connected
+	db = database;
+
+	// 
+	itemsCollection = db.collection('items');
+
+	app.listen(3000, () => {
 	console.log('server is running on port 3000');
-});
+	});
+})
+
+// Create a router than can accept json
+const router = express.Router();
+router.use(bodyParser.json())
+
+// setup connection routes
+router.route('/')
+	.get(function (req, res, next) {
+		itemsCollection.find().toArray(function(err, docs) {
+			res.send({
+				status: 'Items Found',
+				items: docs
+			});
+		})
+	})
+
+	.post(function(req, res, next) {
+		let item = req.body;
+		itemsCollection.insert(item, function(err, docs) {
+			res.send({
+				status: 'Item Added',
+				itemId: item._id
+			});
+		});
+	})
+
+// setup the item routes
+router.route('/:id')
+	.delete(function(req, res, next) {
+		let id = req.params['id'];
+		let lookup = { _id: new mongodb.ObjectID(id) };
+
+		itemsCollection.remove(lookup, function(err, result) {
+			res.send({ status: 'Item Cleared'})
+		})
+	})
+
+app.use(express.static(__dirname + '/public'))
+	.use('/todo', router)
