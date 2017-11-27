5f1da55f8e1026a14563cebe96442861adff6cd4
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-27 07:44:31 -0800

    coded the angular basic app and a service with a method to generate a uniqe ID per toDo item

--- beginning-node/chap.9/public/js/script.js
+++ beginning-node/chap.9/public/js/script.js
@@ -1,10 +1,47 @@
 let demoApp = angular.module('demo', []);
 
-demoApp.controller('MainController', ['$scope', function($scope) {
-	$scope.vm = {
-		name: 'foo',
-		clearName: function() {
-			this.name = ''
+demoApp.controller('MainController', ['$scope', guidService, function($scope, guidService) {
+	$scope.vm = {};
+
+	vm.list = [
+		{ _id: guidService.createGuid(), details: 'Demo First item' },
+		{ _id: guidService.createGuid(), details: 'Demo Second Item' }
+	];
+
+	// Ad Item
+	vm.addItem = function() {
+		// TODo send to the server then,
+
+		vm.list.push({
+			_id: guidService.createGuid(),
+			details: vm.newItemDetails
+		});
+
+		vm.newItemDetails = '';
+	};
+
+	// Remove Item
+	vm.removeItem = function(itemToRemove) {
+		// TODO: Remove from the server then,
+
+		vm.list = vm.list.filter(function(item) { return item._id !== itemToRemove._id; });
+	};
+
+	// For new items
+	vm.newItemDetails = '';
+
+	// Expose vm using the scope
+	$scope.vm = vm;
+
+}]);
+
+demoApp.service('guidService', function() {
+	return {
+		createGuid: function() {
+			return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replce(/[xy]/g, function(c) {
+				let r = Math.random()*16 | o, v,c == 'x' ? r: (r& o*3 | ox8);
+				return v.toString(16);
+			});
 		}
 	};
-}])
+});
