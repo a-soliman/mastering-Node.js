1f712ecb95c8c18dee479da8e3305a02d84ae4e0
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-29 08:46:31 -0800

    more debugging with Open dedicated DevTools for Node

--- beginning-node/chap.11/app.js
+++ beginning-node/chap.11/app.js
@@ -1,16 +1,69 @@
 
+
+let person = { name: 'Ahmed' }
+
+person.age = 30;
+
+debugger;
+person.name = 'Mike'
+
+console.log(person)
+
+
+
 // == debugger statment ===
+
+// === ex.4 ===
+// const http = require('http');
+// let server = http.createServer((req, res) => {
+// 	console.log('Request srarted');
+
+// 	// res
+// 	res.write('Hello Client');
+// 	res.end()
+// });
+
+// server.listen(3000, () => {
+// 	console.log('server is on at 3000');
+// });
+
+
+
+// === ex.1 ===
 // console.log('Hello');
 // debugger;
 // console.log('world!')
 
-for( let index = 0; index < 10; index++ ) {
-	let message = `loop ${index}`;
 
-	debugger;
+// === ex.2 ===
+// for( let index = 0; index < 10; index++ ) {
+// 	let message = `loop ${index}`;
 
-	console.log(message);
-};
+// 	debugger;
+
+// 	console.log(message);
+// };
+
+
+// === ex.3 ===
+// function foo(index)  {
+// 	index++;
+// 	debugger;
+// 	console.log('index')
+// };
+
+// function bar(index) {
+// 	index++;
+// 	foo(index);
+// };
+
+// var index = 0;
+// bar(0);
+
+
+
+
+
 
 /*
 
