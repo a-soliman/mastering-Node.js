6327d34560e498366aba74c941cd330e4ac7d6c7
Ahmed Soliman <ahmed.soliman@programmer.net>
2017-11-29 07:23:00 -0800

    more with console.time

--- beginning-node/chap.11/app.js
+++ beginning-node/chap.11/app.js
@@ -3,3 +3,13 @@ console.time();
 setTimeout(() => {
 	console.timeEnd()
 }, 700);
+
+console.time('first');
+setTimeout(() => {
+	console.timeEnd('first');
+}, 1000);
+
+console.time('second');
+setTimeout(() => {
+	console.timeEnd('second');
+}, 320);
